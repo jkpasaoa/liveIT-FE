ea82fa055c562f577f02c7c09872c72359d45442
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-08 16:34:17 -0400

    setup app.js,  navbar, home

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      {/* <h1>
+        <Link to="/">Snacks</Link>
+      </h1> */}
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/Pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <h3>To the Live It App!</h3>
+    </div>
+  );
+}
+
+export default Home;
