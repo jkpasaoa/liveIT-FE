3f6c36aaa66e2b05d5487ea0cb23eed9fcc31789
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-08 16:39:20 -0400

    added error page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+//PAGES
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+
+//COMPONENTS
+import NavBar from "./Components/NavBar"
 
 function App() {
   return (
     <div>
-      
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </main>
+      </Router>
+
     </div>
   );
 }
---
+++ src/Pages/Error.js
@@ -0,0 +1,9 @@
+function Error() {
+  return (
+    <div>
+      <h1>Sorry, no page found</h1>
+    </div>
+  );
+}
+
+export default Error;
