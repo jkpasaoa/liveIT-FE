951b8f79b560c3e5fb734f43972f546f46fb41fe
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-05-11 00:31:14 -0400

    branding

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,6 +5,7 @@ function Home() {
     <div style={{ textAlign: "center" }}>
       <h2>Welcome</h2>
       <h3>To the Live It App!</h3>
+      <p>"Why Die-T When You Can Live It?"</p>
       <Snacks />
     </div>
   );
