0a74f4e441a820466945c06bf6a10dcc1f862be1
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-05-11 00:29:04 -0400

    centering

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import Snacks from "../Components/Snacks";
 
 function Home() {
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h2>Welcome</h2>
       <h3>To the Live It App!</h3>
       <Snacks />
