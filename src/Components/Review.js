06d4657e4e1ffecc99f10a2b33ff67650e66d3c6
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-12 16:21:21 -0400

    added html, and functionality to review.js

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -14,17 +14,28 @@ function Review({ review, handleDelete, handleEdit }) {
       {
         viewEditForm ? (
           <ReviewForm
-          reviewDetails={review}
-          toggleView={toggleView}
-          handleEdit={handleEdit}
+            reviewDetails={review}
+            toggleView={toggleView}
+            handleEdit={handleEdit}
           />
         ) : (
           <div>
-
+            <h4>{review.title}
+              <span>
+                {review.rating}
+              </span>
+            </h4>
+            <h5>
+              {review.reviewer}
+            </h5>
+            <p>
+              {review.content}
+            </p>
           </div>
-
         )
       }
+      <button onClick={() => handleDelete(review.id)
+      }>Delete</button>
     </div>
   )
 }
