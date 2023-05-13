abe679460aa2d8ad0e25722f8aca3cf9b5b2c4f2
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-12 20:31:50 -0400

    fixed up readme.md on front end and possible darkmode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { useState } from "react";
 
 //PAGES
 import Home from "./Pages/Home";
@@ -12,26 +13,28 @@ import Edit from "./Pages/Edit";
 import NavBar from "./Components/NavBar";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
+// import DarkModeToggle from "./Components/DarkMode";
 
 function App() {
+  // const [darkMode, setDarkMode] = useState(false);
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/snacks" element={<Index />} />
-            <Route path="/snacks/:id" element={<Show />} />
-            <Route path="/snacks/new" element={<New />} />
-            <Route path="/snacks/:id/edit" element={<Edit />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
-
+        <Router>
+          <NavBar />
+          {/* <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} /> */}
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/snacks" element={<Index />} />
+              <Route path="/snacks/:id" element={<Show />} />
+              <Route path="/snacks/new" element={<New />} />
+              <Route path="/snacks/:id/edit" element={<Edit />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </main>
+          <Footer />
+        </Router>
     </div >
   );
 }
