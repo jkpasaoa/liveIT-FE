910e6aafeda999b28a06ccf124b117a748ee81e1
jkpasaoa <jacqueline.pasaoa@gmail.com>
2023-05-14 02:22:45 -0400

    stylized add new review form

--- src/Components/ReviewForm.js
+++ src/Components/ReviewForm.js
@@ -42,52 +42,65 @@ function ReviewForm(props) {
     <div className="Edit" style={{ maxWidth: '500px', margin: '0 auto', paddingTop: '50px' }}>
       {props.children}
       <form onSubmit={handleSubmit}>
-        <label htmlFor="reviewer">Name:</label>
-        <input
-          id="reviewer"
-          value={review.reviewer}
-          type="text"
-          onChange={handleTextChange}
-          placeholder="Your name"
-          required
-        />
-        <label htmlFor="title">Title:</label>
-        <input
-          id="title"
-          type="text"
-          required
-          value={review.title}
-          onChange={handleTextChange}
-        />
-        <label htmlFor="rating">Rating:</label>
-        <input
-          id="rating"
-          type="number"
-          name="rating"
-          min="0"
-          max="5"
-          step="1"
-          value={review.rating}
-          onChange={handleTextChange}
-          required
-        />
-        <label htmlFor="content">Review:</label>
-        <textarea
-          id="content"
-          type="text"
-          name="content"
-          value={review.content}
-          placeholder="What do you think..."
-          onChange={handleTextChange}
-          required
-        />
+        <div className="form-group">
+          <label htmlFor="reviewer">Name:</label>
+          <input
+            id="reviewer"
+            value={review.reviewer}
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Your name"
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="title">Title:</label>
+          <input
+            id="title"
+            type="text"
+            required
+            value={review.title}
+            onChange={handleTextChange}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="rating">Rating:</label>
+          <input
+            id="rating"
+            type="number"
+            name="rating"
+            min="0"
+            max="5"
+            step="1"
+            value={review.rating}
+            onChange={handleTextChange}
+            className="form-control"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="content">Review:</label>
+          <textarea
+            id="content"
+            type="text"
+            name="content"
+            value={review.content}
+            placeholder="What do you think..."
+            onChange={handleTextChange}
+            className="form-control"
+            required
+          />
+        </div>
 
         <br />
 
-        <input type="submit" />
+        <input type="submit" className="btn btn-primary" />
       </form>
     </div>
   );
 }
 
 export default ReviewForm;
+
