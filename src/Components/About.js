72845ac9d4432b6fb8e24235224e31ef3abafcf4
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-05-15 13:57:30 -0400

    working on dark mode

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ThemeContext from './ThemeContext';
 // import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Image } from "react-bootstrap";
---
+++ src/Components/ThemeContext.js
@@ -0,0 +1,20 @@
+// ThemeContext.js
+import { createContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeContext;
